c09e16a23a7b36032db8db232afd02d710441541
lalu adam <adamlalu22@gmail.com>
2024-11-20 10:07:36 +0800

    baru login form nya selesai

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+  
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+  
+      const username = loginForm.username.value;
+      const password = loginForm.password.value;
+  
+      
+      if (username === "user" && password === "pass") {
+        alert("Login successful!");
+        
+        window.location.href = "dashboard.html"; 
+      } else {
+        alert("Incorrect username or password.");
+      }
+    });
+  });
+  
